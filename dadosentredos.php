8c9be3b4c6b50a164972a6dddeb80a1abb2a5f18
Giuliana <gcastillo@safareyes.es>
2024-10-17 10:35:16 +0200

    agregado el dado a eliminar y la tirada con este eliminado

--- dadosentredos.php
+++ dadosentredos.php
@@ -27,11 +27,28 @@
     echo "<div>";
     echo "<h2>Tirada de 6 dados</h2>";
     foreach ($tiradaInicial as $dado) {
-        echo "<img src='imagenes/{$dado}.svg' alt='Dado {$dado}' class='dice'>";
+        echo "<img src='img/{$dado}.svg' alt='Dado {$dado}' class='dice'>";
     }
     echo "</div>";
 
-    // Calcular suma de la tirada inicial
+    // Dado a eliminar
+    echo "<div>";
+    echo "<h2>Dado a eliminar</h2>";
+    echo "<img src='img/{$dadoSuelto}.svg' alt='Dado {$dadoSuelto}' class='dice'>";
+    echo "</div>";
+
+    // Filtrar tirada inicial para que no aparezca el eliminado.
+    $tiradaFiltrada = array_filter($tiradaInicial, function($dado) use ($dadoSuelto) {
+        return $dado !== $dadoSuelto;
+    });
+
+    // Mostrar tirada filtrada sin dado eliminado
+    echo "<div>";
+    echo "<h2>Dados restantes</h2>";
+    foreach ($tiradaFiltrada as $dado) {
+        echo "<img src='img/{$dado}.svg' alt='Dado {$dado}' class='dice'>";
+    }
+    echo "</div>";
     ?>
 </body>
 </html>
