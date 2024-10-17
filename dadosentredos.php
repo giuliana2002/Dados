f22e7356e3e7b6c1004ec99c775cf5a0da07f5b4
Giuliana <92090869+giuliana2002@users.noreply.github.com>
2024-10-17 10:06:20 +0200

    Update dadosentredos .php
    agregar html y algo de css

---
+++ dadosentredos.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Tirada de Dados entre Alvaro y Giuliana</title>
+    <style>
+        .dice {
+            width: 100px;
+            height: 100px;
+        }
+    </style>
+</head>
+<body>
+    <h1>Tirada de dados</h1>
